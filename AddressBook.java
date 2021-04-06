72e6273972a362d344c4d749a11b089c584ba3f4
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-06 22:29:42 +0530

    Added: Edit the details of person

--- AddressBook.java
+++ AddressBook.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class AddressBook
 {
-	public String name, address, city;
+	public String first_name,last_name,address, city;
 	public String state,zip,phone,email;
 	
 	
@@ -12,8 +12,10 @@ public class AddressBook
 		
 		System.out.println();
 		System.out.println("Enter Details of New person");
-		System.out.println("Enter the Name: ");
-		person.name = sc.nextLine();
+		System.out.println("Enter the First Name: ");
+		person.first_name = sc.nextLine();
+		System.out.println("Enter the Last Name: ");
+		person.last_name = sc.nextLine();
 		System.out.println("Enter the Address: ");
 		person.address = sc.nextLine();
 		System.out.println("Enter the Name of city: ");
@@ -31,22 +33,49 @@ public class AddressBook
 	public void showDetails(AddressBook person)
 	{
 		System.out.println();
-		System.out.println("Details of " + person.name + " person");
-		System.out.print("Name: "+person.name+ "\nAddress: "+person.address+ "\nCity: "+person.city);
+		System.out.println("Details of " + person.first_name + " person");
+		System.out.print("First Name: "+person.first_name+"Last Name: "+person.last_name+ "\nAddress: "+person.address+ "\nCity: "+person.city);
 		System.out.print("\nstate: "+person.state+ "\nzip code: "+zip+ "\nPhone no.: "+person.phone+ "\nEmail: "+person.email);
 		System.out.println();
 	}
 	
-	public void editDetails(String[] args,AddressBook person)
-	{
-		person.name = args[0];
-		person.address = args[1];
-		person.city = args[2];
-		person.state= args[3];
-		person.zip = args[4];
-		person.phone = args[5];
-		person.email = args[6];
+	public void editDetails(AddressBook person)
+	{	Scanner edit = new Scanner(System.in);
+		System.out.println("Do you want to edit details of" +person.first_name+ " ? select y/n");
+		String check = edit.next();
+		if(check.equalsIgnoreCase("y"))
+		{
+			System.out.println("Enter the First name of person");
+			String name = edit.next();
+
+			if(name.equalsIgnoreCase(person.first_name))
+			{	
+				System.out.println();
+				System.out.println("Edit Details of person");
+				System.out.println("Enter the First Name: ");
+				person.first_name = edit.nextLine();
+				System.out.println("Enter the Last Name: ");
+				person.last_name = edit.nextLine();
+				System.out.println("Enter the Address: ");
+				person.address = edit.nextLine();
+				System.out.println("Enter the Name of city: ");
+				person.city = edit.nextLine();
+				System.out.println("Enter the Name of state");
+				person.state = edit.nextLine();
+				System.out.println("Enter the Name of zip code ");
+				person.zip = edit.nextLine();
+				System.out.println("Enter the Phone Number: ");
+				person.phone = edit.nextLine();
+				System.out.println("Enter the Email address: ");
+				person.email = edit.nextLine();
+			}
+			else
+				System.out.println("not any person of available og this name");
+		}
+		else
+			System.out.println("you have not any changes"); 
 	}
+
 	
 	
 	
@@ -56,13 +85,14 @@ public class AddressBook
 		AddressBook atharva = new AddressBook();
 		AddressBook ram = new AddressBook();
 		
-		atharva.setDetails(atharva);
-		ram.setDetails(ram);
+		atharva.setDetails(atharva);   // Adding new details of atharva object
+		atharva.showDetails(atharva);  // Display details of atharva
+
 		
-		atharva.showDetails(atharva);
-		ram.showDetails(ram);
+		atharva.editDetails(atharva); // edit details of atharva object
+		atharva.showDetails(atharva); 
 		
-		atharva.editDetails(args,atharva);
-		atharva.showDetails(atharva);
+		
+		
 	}
 }
